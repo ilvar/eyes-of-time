f30963a06d8b3c21e85640139d0e966bd59a5236
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-04-11 10:57:46 -0500

    Sttings fix

--- eot/wsgi.py
+++ eot/wsgi.py
@@ -11,8 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eot.settings")
+
+from whitenoise.django import DjangoWhiteNoise
 
 application = DjangoWhiteNoise(get_wsgi_application())

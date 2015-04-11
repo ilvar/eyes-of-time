60fc5dc0b7ffa05e3636e5af1eb80d4373c515bb
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-04-11 16:14:08 -0500

    Mobile SSO

--- eot/middleware.py
+++ eot/middleware.py
@@ -3,13 +3,15 @@ from django.shortcuts import redirect
 
 class MobileSSOMiddleware(object):
     def process_request(self, request):
-        if request.GET.get('next'):
-            request.session['mobile'] = request.GET.get('next')
+        mobile_redirect = request.GET.get('mobile')
+        if mobile_redirect:
+            print 'mobile_url put', mobile_redirect
+            request.session['mobile'] = mobile_redirect
             return redirect('/accounts/login/facebook/?next=/')
 
         if request.path == '/' or request.path == '/accounts/profile/':
             mobile_url = request.session.get('mobile')
-            print 'mobile_url', mobile_url
+            print 'mobile_url read', mobile_url
             if mobile_url:
                 del request.session['mobile']
                 return redirect(mobile_url)

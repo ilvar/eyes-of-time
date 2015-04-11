f0b54b479cfa85feb71e73086bcbf7089622f636
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-04-11 15:57:36 -0500

    Mobile SSO

--- eot/middleware.py
+++ eot/middleware.py
@@ -6,7 +6,7 @@ class MobileSSOMiddleware(object):
         if request.GET.get('next'):
             request.session['mobile'] = request.GET.get('next')
 
-        if request.GET.get('logged'):
+        if request.path == '/':
             mobile_url = request.session.get('mobile')
             if mobile_url:
                 del request.session['mobile']

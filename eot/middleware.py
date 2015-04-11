eac0da7bffc02939f44b6155f40f5760b2d3aa91
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-04-11 16:10:43 -0500

    Mobile SSO

--- eot/middleware.py
+++ eot/middleware.py
@@ -7,7 +7,7 @@ class MobileSSOMiddleware(object):
             request.session['mobile'] = request.GET.get('next')
             return redirect('/accounts/login/facebook/?next=/')
 
-        if request.path == '/':
+        if request.path == '/' or request.path == '/accounts/profile/':
             mobile_url = request.session.get('mobile')
             if mobile_url:
                 del request.session['mobile']
